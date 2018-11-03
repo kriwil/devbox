93dd2cf1518b3a0803e41a9ff070db847821458c
Aldiantoro Nugroho <kriwil@gmail.com>
2018-11-03 07:25:25 +0700

    script to install vim and tmux

--- vim.sh
+++ vim.sh
@@ -2,9 +2,9 @@ WORKSPACE=$HOME/Workspace
 DOTFILES_DIR=$WORKSPACE/dotfiles
 NVIM_CONFIG=$HOME/.config/nvim
 
-sudo apt-add-repository -y ppa:neovim-ppa/stable
-sudo apt-get update -y
-sudo apt-get install -y python-dev python-pip python3-dev python3-pip neovim python3-neovim python-neovim
+# sudo apt-add-repository -y ppa:neovim-ppa/stable
+# sudo apt-get update -y
+# sudo apt-get install -y python-dev python-pip python3-dev python3-pip neovim python3-neovim python-neovim
 
 mkdir -p $NVIM_CONFIG
 ln -s $DOTFILES_DIR/vimrc $HOME/.config/nvim/init.vim
