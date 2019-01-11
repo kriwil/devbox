5bcf5120653dfc18e25d77e07df79631c7f93c31
Aldiantoro Nugroho <kriwil@gmail.com>
2019-01-11 12:24:20 +0000

    add script to create swap

---
+++ swap.sh
@@ -0,0 +1,7 @@
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo swapon --show
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
